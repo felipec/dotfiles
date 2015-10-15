0174321459c2d50956f7f872eced7e2682b4b189
Felipe Contreras <felipe.contreras@gmail.com>
2015-10-15 02:12:30 -0500

    scripts: improve gmail count tool
    * And remove password
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- .scripts/gmail.rb
+++ .scripts/gmail.rb
@@ -5,7 +5,7 @@ require 'net/https'
 require 'rexml/document'
 
 $user = 'felipe.contreras@gmail.com'
-$password = 'dzjbfyobcmshhjom'
+$password = 'password'
 
 def get_count(http, name, id=nil)
   id ||= name
@@ -21,12 +21,12 @@ end
 
 http = Net::HTTP.new('mail.google.com', 443)
 http.use_ssl = true
-http.start do |http|
-  get_count(http, 'priority', '%5Eiim')
-  get_count(http, 'inbox', '')
-  get_count(http, 'to-do')
-  get_count(http, 'git')
-  get_count(http, 'notmuch')
-  get_count(http, 'gst-dsp')
-  get_count(http, 'isync')
+begin
+  http.start do |http|
+    get_count(http, 'priority', '%5Eiim')
+    get_count(http, 'inbox', '')
+    get_count(http, 'to-do')
+  end
+rescue SocketError
+  exit 1
 end
